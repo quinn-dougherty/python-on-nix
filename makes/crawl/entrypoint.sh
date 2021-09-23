82337e83ee1b5f230cd962814aac18bba2e1bbd6
Kevin Amado <kamadorueda@gmail.com>
2021-09-23 01:14:50 -0500

    feat(doc): single template

--- makes/crawl/entrypoint.sh
+++ makes/crawl/entrypoint.sh
@@ -7,8 +7,8 @@ function main {
     && rm -rf data/projects/* \
     && for project in "${!projects_markdowns[@]}"; do
       : && echo --- > "content/projects/${project}.md" \
+        && echo "project: ${project}" >> "content/projects/${project}.md" \
         && echo --- >> "content/projects/${project}.md" \
-        && echo "{{% project \"${project}\" %}}" >> "content/projects/${project}.md" \
         && copy "${projects_markdowns[$project]}" "data/projects/${project}.json" \
         || return 1
     done
