5805c311f2c244201d51a8c68f34a874e9a6e630
Kevin Amado <kamadorueda@gmail.com>
2021-09-24 01:38:38 -0500

    feat(doc): single layout

--- makes.nix
+++ makes.nix
@@ -3,10 +3,13 @@
 }:
 {
   inputs = {
-    pythonOnNixSrc = builtins.fetchGit {
-      ref = "main";
-      url = "https://github.com/kamadorueda/python-on-nix";
-    };
-    pythonOnNix = import inputs.pythonOnNixSrc;
+    pythonOnNixRef = "main";
+    pythonOnNixRev = "910d3f6554acd4a4ef0425ebccd31104dccb283c";
+    pythonOnNixUrl = "https://github.com/kamadorueda/python-on-nix";
+    pythonOnNix = import (builtins.fetchGit {
+      ref = inputs.pythonOnNixRef;
+      rev = inputs.pythonOnNixRev;
+      url = inputs.pythonOnNixUrl;
+    });
   };
 }
